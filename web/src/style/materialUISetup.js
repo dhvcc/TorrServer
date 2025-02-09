b0597fc0c184c12df9bd69d45705aee5f6210562
dhvcc <alexey.artishevskiy@gmail.com>
2025-02-09 23:17:44 +0100

    Fix build and use auto theme by default
    See https://github.com/YouROK/TorrServer/pull/495
    And https://github.com/YouROK/TorrServer/pull/494

--- web/src/style/materialUISetup.js
+++ web/src/style/materialUISetup.js
@@ -26,11 +26,11 @@ export const lightTheme = createTheme({
 
 export const useMaterialUITheme = () => {
   // PATCH START Use auto mode by default
-  const savedThemeMode = localStorage.getItem('themeMode') || THEME_MODES.AUTO
+  const savedThemeMode = localStorage.getItem('themeMode')
   // PATCH END
   const isSystemModeDark = useMediaQuery('(prefers-color-scheme: dark)')
   const [isDarkMode, setIsDarkMode] = useState(savedThemeMode === 'dark' || isSystemModeDark)
-  const [currentThemeMode, setCurrentThemeMode] = useState(savedThemeMode || THEME_MODES.LIGHT)
+  const [currentThemeMode, setCurrentThemeMode] = useState(savedThemeMode || THEME_MODES.AUTO)
 
   const updateThemeMode = mode => {
     setCurrentThemeMode(mode)
